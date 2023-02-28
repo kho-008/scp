f9efdf195aa88a56681bde468182b3771b718bd1
kho-008 <shigeo_wake@kitahamagm.co.jp>
2023-02-28 11:21:33 +0900

    add and modified

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 - [scpコマンドの基本と使い方｜Windowsへコピーする実例付](https://itc.tokyo/linux/scp-command/)
 - [bashでscpするシェルスクリプト - Qiita](https://qiita.com/popolon31/items/abb017eb6b8c6ad21e1a)
 - [scpコマンドを使ったサーバー間のファイルのやり取り – skillup](https://skill-up-engineering.com/2017/08/24/post-3263/#i-2)
+
+### ログ
+- [perl - Shell Scripting : Unable to scp output to log file - Stack Overflow](https://stackoverflow.com/questions/8065248/shell-scripting-unable-to-scp-output-to-log-file)
+- [Web エンジニアの自分のための備忘録: scpの通信ログをログファイルに残す方法](https://gyagya1111.blogspot.com/2011/04/scp.html)
--- download.sh
+++ download.sh
@@ -1,2 +1,16 @@
-# ローカルにフォルダを作成しないといけない
-scp -i ~/.ssh/id_rsa -r user_name@example.example.jp:/home/user_name/public_html/path/to/file.scss ./to/file.scss
+WORK_DATE=`date +%Y%m%d_%H%M%S`
+
+BACKUP_DIR=backup_$WORK_DATE
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir $BACKUP_DIR
+fi
+
+LOGDIR=log
+if [ ! -d $LOGDIR ]; then
+  mkdir $LOGDIR
+fi
+
+while read FILE_TO_PATH || [ -n "${FILE_TO_PATH}" ]; do
+  mkdir -p "${BACKUP_DIR}/`dirname ${FILE_TO_PATH}`"
+  scp -v -i ~/.ssh/id_rsa_kho -r user_name@example.example.jp:/home/user_name/public_html/path/${FILE_TO_PATH} ./${BACKUP_DIR}/${FILE_TO_PATH} 2>>${LOGDIR}/${WORK_DATE}.log
+done < file_list.txt
